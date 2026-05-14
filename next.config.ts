4b07def2992c9ee9250f4cbee40b32a3ce9e71dc
Andre Pinto <andrepinto@mailfence.com>
2026-05-14 18:55:37 -0300

    feat: updating next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,4 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
   output: "export",
-  basePath: isProd ? "/andre6293.github.io" : "",
-  assetPrefix: isProd ? "/andre6293.github.io/" : "",
+  distDir: "dist",
 };
